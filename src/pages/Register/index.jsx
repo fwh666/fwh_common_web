e725d85517ba00d4ca741765bf73a8cd2341c7b2
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-08-04 15:31:34 +0800

    fix: fetch

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { useState} from 'react'
+import { useState,useEffect} from 'react'
 import '../Login/index.css'
 import { useNavigate,NavLink } from 'react-router-dom'
 import { Input } from 'antd'
+import instance from '../../utils/network'
 /**
  * 注册页面
  * @returns 
@@ -20,10 +21,87 @@ export default function Register() {
     const handleRegist=(event)=>{
         event.preventDefault();
         console.log('注册信息',username,password);
+        // todo-fwh-调用接口返回数据
+        // console.log(event.target.value);
+        
+        //原始-可用调用
+        fetch('/api/students')
+        .then(respose=>respose.text())
+        .then((data)=>{
+            console.log('data',data);
+        })
+        
+
+
+
         setusername('')
         setpassword('')
         alert("注册成功")
     }
+
+
+    // 失效-封装调用
+    // instance.get('/api/students')
+    // .then(respose=>respose.text())
+    // .then(console.log(respose))
+    // .then((res)=>{
+    //     console.log('data',res);
+    // })
+
+
+
+    // instance的post方式
+
+    // const onFinish = () => {
+    //     const {validateFields} = form;
+    //     validateFields()
+    //         .then(values => {
+    //             values.redBall = redArray.map(v => v.value);
+    //             console.log('入参:', values);
+    //             instance.post('/api/fwh/bicolorBall/queryColorBall', {
+    //                 ...values,
+    //             }).then((res = []) => {
+    //                 // console.log('响应:', JSON.stringify(res));
+    //                 console.log('响应:', res);
+    //                 //当前页面回显数据
+    //                 // setResData(JSON.stringify(res));
+    //                 setResData(res);
+    //                 if (res.length > 0) {
+    //                     setShowTable(true);
+    //                 }
+
+    //                 //新页面-透传响应值-回显列表
+    //                 // window.appData= res;
+    //                 // navigate('/view?');
+    //             })
+    //             // console.log('values', values);
+    //         })
+    // };
+
+    //组件挂载形式
+    // const [htmlContent, setHtmlContent] = useState("<div>正在跳转支付宝支付...</div>");
+    // useEffect(() => {
+    //     console.log('组件已经挂载');
+    //     fetch('/api/pay/getWebPay')
+    //         .then(response => response.text())
+    //         .then((data) => {
+    //             console.log('data:', data);
+    //             const formElement = document.createElement("div");
+    //             formElement.style.display = "display:none;";
+    //             formElement.innerHTML = data;//去到from
+    //             if (formElement.firstChild) {
+    //                 formElement.firstChild.target = "_self";
+    //                 document.body.appendChild(formElement);
+    //                 formElement.firstChild.submit();//进行跳转
+    //                 document.body.removeChild(formElement);
+    //             }
+    //         });
+    // }, []);
+    // return (
+    //     <div dangerouslySetInnerHTML={{__html: htmlContent}}/>
+    // );
+
+
 
   return (
     <>
@@ -33,10 +111,10 @@ export default function Register() {
             <form onSubmit={handleRegist}>
                 <div className="form-group">
                     {/* <input onChange={(event)=>setusername(event.target.value)} type="text" name="email" value={username} placeholder="邮箱：example@gmail.com"/> */}
-                    <Input onChange={(event)=>setusername(event.target.value)} type="email" name="email" value={username} placeholder="邮箱：example@gmail.com"/>
+                    <Input onChange={(event)=>setusername(event.target.value)} type="text" name="email" value={username} placeholder="邮箱：example@gmail.com"/>
                 </div>
                 <div className="form-group">
-                    <input onChange={(event)=>setpassword(event.target.value)} id="registerPassword" type="password" name="password" value={password} placeholder="密码：Password"/>
+                    <Input onChange={(event)=>setpassword(event.target.value)} id="registerPassword" type="password" name="password" value={password} placeholder="密码：Password"/>
                 </div>
                 <button id="registerSubmitId" type="submit">注册</button>
             </form>
